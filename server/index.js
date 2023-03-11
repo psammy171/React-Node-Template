fb17b3e38b93d0726d53d9c98a266121d17b6db0
psammy171 <127593704+psammy171@users.noreply.github.com>
2023-03-11 16:39:13 +0530

    Added .env file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path')
-const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const app = express();
 app.use(express.json())
